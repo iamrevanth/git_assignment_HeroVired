7771abafff719de77062a0e28fbb3e581fe1f1e2
iamrevanth <54358377+iamrevanth@users.noreply.github.com>
2025-06-20 23:42:28 +0530

    Update rect-area.py

--- rect-area.py
+++ rect-area.py
@@ -1,7 +1,7 @@
 import math
 class GeometryCalculator:
-	def calculate_circle_area(self, radius):
-		return math.pi * radius ** 2
+	#def calculate_circle_area(self, radius):
+	#	return math.pi * radius ** 2
 
 	def calculate_rectangle_area(self, length, width):
 		return length * width
